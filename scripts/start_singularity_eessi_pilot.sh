41a95d81020e5f9d8f80c096145e23737b362b86
Bob Dröge <b.e.droge@rug.nl>
2021-01-12 13:37:04 +0100

    honor TMPDIR if already set

--- scripts/start_singularity_eessi_pilot.sh
+++ scripts/start_singularity_eessi_pilot.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-mkdir -p /tmp/$USER/{var-lib-cvmfs,var-run-cvmfs,home}
-export SINGULARITY_BIND="/tmp/$USER/var-run-cvmfs:/var/run/cvmfs,/tmp/$USER/var-lib-cvmfs:/var/lib/cvmfs"
-export SINGULARITY_HOME="/tmp/$USER/home:/home/$USER"
+
+# honor $TMPDIR if it is already defined, use /tmp otherwise
+if [ -z $TMPDIR ]; then
+    export WORKDIR=/tmp/$USER
+else
+    export WORKDIR=$TMPDIR/$USER
+fi
+
+mkdir -p ${WORKDIR}/{var-lib-cvmfs,var-run-cvmfs,home}
+export SINGULARITY_BIND="${WORKDIR}/var-run-cvmfs:/var/run/cvmfs,${WORKDIR}/var-lib-cvmfs:/var/lib/cvmfs"
+export SINGULARITY_HOME="${WORKDIR}/home:/home/$USER"
 export EESSI_CONFIG="container:cvmfs2 cvmfs-config.eessi-hpc.org /cvmfs/cvmfs-config.eessi-hpc.org"
 export EESSI_PILOT="container:cvmfs2 pilot.eessi-hpc.org /cvmfs/pilot.eessi-hpc.org"
 singularity shell --fusemount "$EESSI_CONFIG" --fusemount "$EESSI_PILOT" docker://eessi/client-pilot:centos7-$(uname -m)-2020.10
