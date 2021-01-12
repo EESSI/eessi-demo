56477f01d693235695e9b5e24a163ebdd812da84
Bob Dröge <b.e.droge@rug.nl>
2021-01-12 13:37:22 +0100

    remove pilot version from container tag

--- scripts/start_singularity_eessi_pilot.sh
+++ scripts/start_singularity_eessi_pilot.sh
@@ -12,4 +12,4 @@ export SINGULARITY_BIND="${WORKDIR}/var-run-cvmfs:/var/run/cvmfs,${WORKDIR}/var-
 export SINGULARITY_HOME="${WORKDIR}/home:/home/$USER"
 export EESSI_CONFIG="container:cvmfs2 cvmfs-config.eessi-hpc.org /cvmfs/cvmfs-config.eessi-hpc.org"
 export EESSI_PILOT="container:cvmfs2 pilot.eessi-hpc.org /cvmfs/pilot.eessi-hpc.org"
-singularity shell --fusemount "$EESSI_CONFIG" --fusemount "$EESSI_PILOT" docker://eessi/client-pilot:centos7-$(uname -m)-2020.10
+singularity shell --fusemount "$EESSI_CONFIG" --fusemount "$EESSI_PILOT" docker://eessi/client-pilot:centos7-$(uname -m)
