b09ebb04e8efc0cd9f43ed6f3b0d9ebf8640c93c
ocaisa <alan.ocais@cecam.org>
2023-05-17 15:57:10 +0200

    Update install_cvmfs_2.10.1_eessi_AmazonLinux2023.sh

--- scripts/install_cvmfs_2.10.1_eessi_AmazonLinux2023.sh
+++ scripts/install_cvmfs_2.10.1_eessi_AmazonLinux2023.sh
@@ -8,10 +8,8 @@ sudo yum install -y http://ecsft.cern.ch/dist/cvmfs/cvmfs-config/cvmfs-config-de
 sudo yum install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-2.10.1/cvmfs-2.10.1-1.el9.aarch64.rpm
 sudo yum install -y https://github.com/EESSI/filesystem-layer/releases/download/latest/cvmfs-config-eessi-latest.noarch.rpm
 
-sudo service autofs start
-sudo cvmfs_config setup
-
 sudo bash -c "echo 'CVMFS_CLIENT_PROFILE="single"' > /etc/cvmfs/default.local"
 sudo bash -c "echo 'CVMFS_QUOTA_LIMIT=10000' >> /etc/cvmfs/default.local"
 
+sudo service autofs start
 sudo cvmfs_config setup
