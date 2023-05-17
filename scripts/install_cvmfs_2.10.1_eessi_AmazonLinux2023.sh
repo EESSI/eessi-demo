1bdea3b450ba6df52ad1e3e83286e76c7a82d9bc
ocaisa <alan.ocais@cecam.org>
2023-05-17 22:10:17 +0200

    Update install_cvmfs_2.10.1_eessi_AmazonLinux2023.sh

--- scripts/install_cvmfs_2.10.1_eessi_AmazonLinux2023.sh
+++ scripts/install_cvmfs_2.10.1_eessi_AmazonLinux2023.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install CVMFS (no yum repo for Amazon Linux)
+# Install CVMFS (no working yum repo for Amazon Linux)
 sudo yum install -y http://ecsft.cern.ch/dist/cvmfs/cvmfs-config/cvmfs-config-default-latest.noarch.rpm
 sudo yum install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-2.10.1/cvmfs-2.10.1-1.el9.$(uname -m).rpm
 sudo yum install -y https://github.com/EESSI/filesystem-layer/releases/download/latest/cvmfs-config-eessi-latest.noarch.rpm
