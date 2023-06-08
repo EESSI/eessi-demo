65733d81f379a3ccf57bbe04f1c1c2c444514c4e
ocaisa <alan.ocais@cecam.org>
2023-06-08 17:31:38 +0200

    Refactor and improve

--- scripts/test_S1_performance.sh
+++ scripts/test_S1_performance.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
+
 function join_by {
   local d=${1-} f=${2-}
   if shift 2; then
     printf %s "$f" "${@/#/$d}"
   fi
 }
-# Initialise EESSI
-source /cvmfs/pilot.eessi-hpc.org/latest/init/bash > /dev/null
-# Grab the date we do this and use that as out key for json output
-date=$(date -I)
-echo "{ \"$date\":["
-json_array=()
-for s1server in `grep CVMFS_SERVER_URL /etc/cvmfs/domain.d/eessi-hpc.org.conf | grep -o '".*"' | sed 's/"//g' | tr ';' '\n'`; do
+
+function test_S1 {
     # Edit the config file to point to a single S1 option, e.g.,
-    echo 'CVMFS_SERVER_URL="'"$s1server"'"' | sudo tee /etc/cvmfs/domain.d/eessi-hpc.org.local > /dev/null
+    echo 'CVMFS_SERVER_URL="'"$1"'"' | sudo tee /etc/cvmfs/domain.d/eessi-hpc.org.local > /dev/null
     # Reconfigure CVMFS 
     sudo cvmfs_config setup
-    # Wipe the cache and run the example (I used Tensorflow from github.com/EESSI/eessi-demo)
+    # Wipe the cache and run the example (from github.com/EESSI/eessi-demo)
     sudo cvmfs_config wipecache >& /dev/null
-    # Run the TensorFlow example
-    cd ../TensorFlow
+    # Run the example
+    cd ../$2
     # Just print the real time
     realtime=$({ ./run.sh > /dev/null ; } 2> >(grep real | awk '{print $2}'))
     KB_per_sec=$(cvmfs_config stat pilot.eessi-hpc.org | column -t -H 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20 | tail -1)
     # Print json output
-    json_array+=("{\"$s1server\": [{\"time\":\"$realtime\"},{\"speed\":\"${KB_per_sec}KB/sec\"}]}")
+    echo -n "{\"$1\": {\"time\":\"$realtime\",\"speed\":\"${KB_per_sec}\",\"speed_unit\":\"KB/sec\",\"application\":\"$2\"}}" 
+}
+
+# Initialise EESSI
+source /cvmfs/pilot.eessi-hpc.org/latest/init/bash > /dev/null
+# Grab the date we do this and use that as out key for json output
+application="${APPLICATION:-TensorFlow}"
+date=$(date -I)
+json_array=()
+for s1server in `grep CVMFS_SERVER_URL /etc/cvmfs/domain.d/eessi-hpc.org.conf | grep -o '".*"' | sed 's/"//g' | tr ';' '\n'`; do
+json_array+=("$(test_S1 "$s1server" "$application")")
 done
+echo -e "{\"$date\":["
 join_by ,$'\n' "${json_array[@]}"
 echo -e "\n]}"
