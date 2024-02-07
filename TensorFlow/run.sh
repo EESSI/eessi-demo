c71dd814843d6b5cf3e65466c87acfe8e3cc8055
vsc46128 vscuser <vsc46128@node4010.donphan.os>
2024-02-07 16:00:20 +0100

    Make TensorFlow demo compatible with exhisting EESSI repos and versions

--- TensorFlow/run.sh
+++ TensorFlow/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 echo $EESSI_CVMFS_REPO
 if [ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]; then
@@ -6,8 +7,15 @@ if [ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]; then
     if [ $EESSI_VERSION == "2023.06" ]; then
         module load TensorFlow/2.13.0-foss-2023a
     fi
+elif [ $EESSI_CVMFS_REPO == "/cvmfs/pilot.eessi-hpc.org" ]; then
+    if [ $EESSI_PILOT_VERSION == "2021.12" ]; then
+        module load TensorFlow/2.3.1-foss-2020a-Python-3.8.2
+    elif [ $EESSI_PILOT_VERSION == "2023.06" ]; then
+        echo "No TensorFlow module available in ${EESSI_CVMFS_REPO}/versions/$EESSI_PILOT_VERSION"
+        echo "Please use the latest version in the /cvmfs/software.eessi.io repo"
+        exit 1;
+    fi
 fi
 
-"module load TensorFlow/2.3.1-foss-2020a-Python-3.8.2"
 
 time python TensorFlow-2.x_mnist-test.py
