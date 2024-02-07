0133f1fe51faad32d1bc878a1a61bce5273b0627
vsc46128 vscuser <vsc46128@node4010.donphan.os>
2024-02-07 15:40:06 +0100

    update tensorflow script

--- TensorFlow/run.sh
+++ TensorFlow/run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-module load TensorFlow/2.3.1-foss-2020a-Python-3.8.2
+echo $EESSI_CVMFS_REPO
+if [ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]; then
+    echo $EESSI_VERSION
+    if [ $EESSI_VERSION == "2023.06" ]; then
+        module load TensorFlow/2.13.0-foss-2023a
+    fi
+fi
+
+"module load TensorFlow/2.3.1-foss-2020a-Python-3.8.2"
 
 time python TensorFlow-2.x_mnist-test.py
