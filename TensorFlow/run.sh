0a32453830743848cb606a7b4ec8cf8eebd28a4d
vsc46128 vscuser <vsc46128@gligar08.gastly.os>
2024-02-08 14:33:25 +0100

    implement sugested changes see https://github.com/EESSI/eessi-demo/pull/31#issuecomment-1932558286

--- TensorFlow/run.sh
+++ TensorFlow/run.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 set -e
 
-echo $EESSI_CVMFS_REPO
-if [ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]; then
-    echo $EESSI_VERSION
-    if [ $EESSI_VERSION == "2023.06" ]; then
-        module load TensorFlow/2.13.0-foss-2023a
-    fi
-elif [ $EESSI_CVMFS_REPO == "/cvmfs/pilot.eessi-hpc.org" ]; then
-    if [ $EESSI_PILOT_VERSION == "2021.12" ]; then
-        module load TensorFlow/2.3.1-foss-2020a-Python-3.8.2
-    elif [ $EESSI_PILOT_VERSION == "2023.06" ]; then
-        echo "No TensorFlow module available in ${EESSI_CVMFS_REPO}/versions/$EESSI_PILOT_VERSION"
-        echo "Please use the latest version in the /cvmfs/software.eessi.io repo"
-        exit 1;
-    fi
+if [[ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]] && [[ $EESSI_VERSION == "2023.06" ]]; then module load TensorFlow/2.13.0-foss-2023a
+elif [[ $EESSI_CVMFS_REPO == "/cvmfs/pilot.eessi-hpc.org" ]] && [[ $EESSI_PILOT_VERSION == "2021.12" ]]; then module load TensorFlow/2.3.1-foss-2020a-Python-3.8.2
+echo "Don't know which TensorFlow module to load for ${EESSI_CVMFS_REPO}/versions/${EESSI_VERSION}$EESSI_PILOT_VERSION" >&2; exit 1
 fi
 
 
