bdefc1d5f7514a0b61206a684f268174dafe5c19
ocaisa <alan.ocais@cecam.org>
2024-02-22 15:07:40 +0100

    Make the script more flexible in general

--- scripts/install_cvmfs_eessi.sh
+++ scripts/install_cvmfs_eessi.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]; then
-    echo "This script requires sudo privileges to install and configure the CVMFS packages. Please run it with sudo."
-    exit 1
-fi
+# Dry run function
+dry_run() {
+    if [ "$DRY_RUN" = true ]; then
+        echo "sudo $@"
+    else
+        eval "$@"
+    fi
+}
 
 # Gather information about the OS
 . /etc/os-release
 
-echo "Attempting to install EESSI for operating system ($NAME : $VERSION)"
+# Perform actions based on the value of DRY_RUN
+if [ "$EUID" -ne 0 ]; then
+    echo
+    echo "This script requires sudo privileges to actually install and configure the CVMFS packages."
+    echo "If that is what you want, please run it with sudo."
+    echo
+    echo "Performing a dry run only..."
+    echo "An attempt to install EESSI for operating system ($NAME : $VERSION) would require:"
+    echo
+    DRY_RUN=true
+else
+    echo "Attempting to install EESSI for operating system ($NAME : $VERSION):"
+fi
+
 error_msg="Don't know how to handle operating system ($NAME : $VERSION)"
 # Check whether we have a RHEL-like or Debian-like system
 if [[ "${ID_LIKE}" =~ "rhel" ]] || [[ "${ID_LIKE}" =~ "fedora" ]] || [[ "${ID_LIKE}" =~ "centos" ]]
@@ -31,37 +48,37 @@ then
     # Install CVMFS (without a yum repo for Amazon Linux), config file first, then CVMFS itself
     AMAZON_LINUX_CVMFS_VERSION=2.11.2
     AMAZON_LINUX_CVMFS_PACKAGE_VERSION=${AMAZON_LINUX_CVMFS_VERSION}-1
-    yum install -y http://ecsft.cern.ch/dist/cvmfs/cvmfs-config/cvmfs-config-default-latest.noarch.rpm
-    yum install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-${AMAZON_LINUX_CVMFS_VERSION}/cvmfs-libs-${AMAZON_LINUX_CVMFS_PACKAGE_VERSION}.el${rhel_version}."$(uname -m)".rpm
-    yum install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-${AMAZON_LINUX_CVMFS_VERSION}/cvmfs-${AMAZON_LINUX_CVMFS_PACKAGE_VERSION}.el${rhel_version}."$(uname -m)".rpm
+    dry_run "yum install -y http://ecsft.cern.ch/dist/cvmfs/cvmfs-config/cvmfs-config-default-latest.noarch.rpm"
+    dry_run "yum install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-${AMAZON_LINUX_CVMFS_VERSION}/cvmfs-libs-${AMAZON_LINUX_CVMFS_PACKAGE_VERSION}.el${rhel_version}.$(uname -m).rpm"
+    dry_run "yum install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-${AMAZON_LINUX_CVMFS_VERSION}/cvmfs-${AMAZON_LINUX_CVMFS_PACKAGE_VERSION}.el${rhel_version}.$(uname -m).rpm"
   else
     # Assume everything else is RHEL-like (install the yum repo and then cvmfs)
-    yum install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm
-    yum install -y cvmfs
+    dry_run "yum install -y https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm"
+    dry_run "yum install -y cvmfs"
   fi
   # Install the EESSI configuration (not strictly necessary as software.eessi.io ships in the default)
-  yum install -y https://github.com/EESSI/filesystem-layer/releases/download/latest/cvmfs-config-eessi-latest.noarch.rpm
+  dry_run "yum install -y https://github.com/EESSI/filesystem-layer/releases/download/latest/cvmfs-config-eessi-latest.noarch.rpm"
 elif [[ "${ID_LIKE}" =~ "debian" ]] || [[ "${ID}" =~ "debian" ]]
 then
-  apt-get install lsb-release wget
-  wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest_all.deb
-  dpkg -i cvmfs-release-latest_all.deb
-  rm -f cvmfs-release-latest_all.deb
-  apt-get update
-  apt-get install -y cvmfs
+  dry_run "apt-get install lsb-release wget"
+  dry_run "wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest_all.deb"
+  dry_run "dpkg -i cvmfs-release-latest_all.deb"
+  dry_run "rm -f cvmfs-release-latest_all.deb"
+  dry_run "apt-get update"
+  dry_run "apt-get install -y cvmfs"
 
   # Install the EESSI configuration (not strictly necessary as software.eessi.io ships in the default)
-  wget https://github.com/EESSI/filesystem-layer/releases/download/latest/cvmfs-config-eessi_latest_all.deb
-  dpkg -i cvmfs-config-eessi_latest_all.deb
-  rm -f cvmfs-config-eessi_latest_all.deb
+  dry_run "wget https://github.com/EESSI/filesystem-layer/releases/download/latest/cvmfs-config-eessi_latest_all.deb"
+  dry_run "dpkg -i cvmfs-config-eessi_latest_all.deb"
+  dry_run "rm -f cvmfs-config-eessi_latest_all.deb"
 else
   echo "$error_msg"
   exit 1
 fi
 
 # Create a simple configuration
-bash -c "echo 'CVMFS_CLIENT_PROFILE=single' > /etc/cvmfs/default.local"
-bash -c "echo 'CVMFS_QUOTA_LIMIT=10000' >> /etc/cvmfs/default.local"
+dry_run "bash -c \"echo 'CVMFS_CLIENT_PROFILE=single' > /etc/cvmfs/default.local\""
+dry_run "bash -c \"echo 'CVMFS_QUOTA_LIMIT=10000' >> /etc/cvmfs/default.local\""
 
 # Initialise cvmfs
-cvmfs_config setup
+dry_run "cvmfs_config setup"
