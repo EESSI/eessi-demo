6459bcc21475d3317a5112cd34d7e488de59d559
Kenneth Hoste <kenneth.hoste@ugent.be>
2024-05-01 18:59:30 +0200

    fix error message when unknown EESSI version is used in GROMACS demo script

--- GROMACS/run.sh
+++ GROMACS/run.sh
@@ -3,7 +3,7 @@ set -e
 
 if [[ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]] && [[ $EESSI_VERSION == "2023.06" ]]; then module load GROMACS/2024.1-foss-2023b
 elif [[ $EESSI_CVMFS_REPO == "/cvmfs/pilot.eessi-hpc.org" ]] && [[ $EESSI_PILOT_VERSION == "2021.12" ]]; then module load GROMACS/2020.1-foss-2020a-Python-3.8.2
-else echo "Don't know which TensorFlow module to load for ${EESSI_CVMFS_REPO}/versions/${EESSI_VERSION}$EESSI_PILOT_VERSION" >&2; exit 1
+else echo "Don't know which GROMACS module to load for ${EESSI_CVMFS_REPO}/versions/${EESSI_VERSION}$EESSI_PILOT_VERSION" >&2; exit 1
 fi
 
 if [ ! -f GROMACS_TestCaseA.tar.gz ]; then
