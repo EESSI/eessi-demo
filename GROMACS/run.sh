541169555840b653da06078b11c2cff1efa1aff1
ocaisa <alan.ocais@cecam.org>
2024-05-01 17:21:14 +0200

    Update GROMACS demo for `software.eessi.io`

--- GROMACS/run.sh
+++ GROMACS/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+set -e
 
-module load GROMACS/2020.1-foss-2020a-Python-3.8.2
+if [[ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]] && [[ $EESSI_VERSION == "2023.06" ]]; then module load GROMACS/2024.1-foss-2023b
+elif [[ $EESSI_CVMFS_REPO == "/cvmfs/pilot.eessi-hpc.org" ]] && [[ $EESSI_PILOT_VERSION == "2021.12" ]]; then module load GROMACS/2020.1-foss-2020a-Python-3.8.2
+else echo "Don't know which TensorFlow module to load for ${EESSI_CVMFS_REPO}/versions/${EESSI_VERSION}$EESSI_PILOT_VERSION" >&2; exit 1
+fi
 
 if [ ! -f GROMACS_TestCaseA.tar.gz ]; then
   curl -OL https://repository.prace-ri.eu/ueabs/GROMACS/1.2/GROMACS_TestCaseA.tar.gz
