e3d71e2a2bc091e047cedbe7a8605c8ce6b02116
vsc46128 vscuser <vsc46128@gligar08.gastly.os>
2024-05-02 09:32:04 +0200

    change names of scripts

--- OpenFOAM/run.sh
+++ OpenFOAM/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]] && [[ $EESSI_VERSION == "2023.06" ]]; then module load OpenFOAM/11-foss-2023a
-./bike_OpenFOAM_11.sh
+./bike_OpenFOAM_v11.sh
 elif [[ $EESSI_CVMFS_REPO == "/cvmfs/pilot.eessi-hpc.org" ]] && [[ $EESSI_PILOT_VERSION == "2021.12" ]]; then module load OpenFOAM/8-foss-2020a
-./bike_OpenFOAM8.sh
+./bike_OpenFOAM_v8.sh
 elif [[ $EESSI_CVMFS_REPO == "/cvmfs/pilot.eessi-hpc.org" ]] && [[ $EESSI_PILOT_VERSION == "2023.06" ]]
 then echo There is no demo for OpenFOAM in "/cvmfs/pilot.eessi-hpc.org/versions/2023.06". Please use the EESSI production repo "/cvmfs/software.eessi.io".
 exit 1;
