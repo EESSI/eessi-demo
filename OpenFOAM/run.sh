4692bb521ccd5f15d0fcdda97621b12f4d427f72
Lara Ramona Peeters <49882639+laraPPr@users.noreply.github.com>
2024-05-02 12:47:42 +0200

    Update OpenFOAM/run.sh
    Co-authored-by: ocaisa <alan.ocais@cecam.org>

--- OpenFOAM/run.sh
+++ OpenFOAM/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [[ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]] && [[ $EESSI_VERSION == "2023.06" ]]; then module load OpenFOAM/11-foss-2023a
+# Allow oversubscription for OpenMPI since the workload is small and meant as proof of principle only 
 export OMPI_MCA_rmaps_base_oversubscribe=true
+
+if [[ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]] && [[ $EESSI_VERSION == "2023.06" ]]; then module load OpenFOAM/11-foss-2023a
 ./bike_OpenFOAM_v11.sh
 elif [[ $EESSI_CVMFS_REPO == "/cvmfs/pilot.eessi-hpc.org" ]] && [[ $EESSI_PILOT_VERSION == "2021.12" ]]; then module load OpenFOAM/8-foss-2020a
-export OMPI_MCA_rmaps_base_oversubscribe=true
 ./bike_OpenFOAM_v8.sh
 elif [[ $EESSI_CVMFS_REPO == "/cvmfs/pilot.eessi-hpc.org" ]] && [[ $EESSI_PILOT_VERSION == "2023.06" ]]
 then echo There is no demo for OpenFOAM in "/cvmfs/pilot.eessi-hpc.org/versions/2023.06". Please use the EESSI production repo "/cvmfs/software.eessi.io".
