d19211fe8950481f419ea0f4afe33c17f2a575da
Alan O'Cais <alan.ocais@cecam.org>
2024-05-08 23:12:41 +0200

    Use client container image

--- scripts/start_singularity_eessi.sh
+++ scripts/start_singularity_eessi.sh
@@ -11,4 +11,4 @@ mkdir -p ${WORKDIR}/{var-lib-cvmfs,var-run-cvmfs,home}
 export SINGULARITY_BIND="${WORKDIR}/var-run-cvmfs:/var/run/cvmfs,${WORKDIR}/var-lib-cvmfs:/var/lib/cvmfs"
 export SINGULARITY_HOME="${WORKDIR}/home:/home/$USER"
 export EESSI_REPO="container:cvmfs2 software.eessi.io /cvmfs/software.eessi.io"
-singularity shell --fusemount "$EESSI_REPO" docker://ghcr.io/eessi/build-node:debian11
+singularity shell --fusemount "$EESSI_REPO" docker://ghcr.io/eessi/client:centos7
