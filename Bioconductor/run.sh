79697b6903b54a81afebfb4bb8c1f40c8c52b180
Alan O'Cais <alan.ocais@cecam.org>
2024-05-08 22:20:29 +0200

    Remove all references to the pilot repository

--- Bioconductor/run.sh
+++ Bioconductor/run.sh
@@ -2,7 +2,6 @@
 set -e
 
 if [[ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]] && [[ $EESSI_VERSION == "2023.06" ]]; then module load R-bundle-Bioconductor/3.16-foss-2022b-R-4.2.2
-elif [[ $EESSI_CVMFS_REPO == "/cvmfs/pilot.eessi-hpc.org" ]] && [[ $EESSI_PILOT_VERSION == "2021.12" ]]; then module load R-bundle-Bioconductor/3.11-foss-2020a-R-4.0.0
 else echo "Don't know which Bioconductor module to load for ${EESSI_CVMFS_REPO}/versions/${EESSI_VERSION}$EESSI_PILOT_VERSION" >&2; exit 1
 fi
 
--- OpenFOAM/run.sh
+++ OpenFOAM/run.sh
@@ -4,14 +4,6 @@ if [[ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]] && [[ $EESSI_VERSION ==
     echo "Running demo for OpenFOAM v11 ..."
     export OMPI_MCA_rmaps_base_oversubscribe=true
     ./bike_OpenFOAM_v11.sh
-elif [[ $EESSI_CVMFS_REPO == "/cvmfs/pilot.eessi-hpc.org" ]] && [[ $EESSI_PILOT_VERSION == "2021.12" ]]; then
-    echo "Running demo for OpenFOAM v8 ..."
-    export OMPI_MCA_rmaps_base_oversubscribe=true
-    ./bike_OpenFOAM_v8.sh
-elif [[ $EESSI_CVMFS_REPO == "/cvmfs/pilot.eessi-hpc.org" ]] && [[ $EESSI_PILOT_VERSION == "2023.06" ]]; then 
-    echo There is no demo for OpenFOAM in "/cvmfs/pilot.eessi-hpc.org/versions/2023.06". <&2
-    echo Please use the EESSI production repo "/cvmfs/software.eessi.io".<&2
-    exit 1;
 else 
     echo "Don't know which OpenFOAM module to load for ${EESSI_CVMFS_REPO}/versions/${EESSI_VERSION}$EESSI_PILOT_VERSION" >&2
     exit 1
