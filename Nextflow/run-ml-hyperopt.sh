08a2304a2c9cf6342059969a95d15294c9fd6c75
Bob Dröge <b.e.droge@rug.nl>
2025-05-02 16:39:12 +0200

    add Nextflow examples

---
+++ Nextflow/run-ml-hyperopt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+if [[ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]] && [[ $EESSI_VERSION == "2023.06" ]]; then
+    module load Nextflow/23.10.0
+    module load scikit-learn/1.4.0-gfbf-2023b
+    module load matplotlib/3.8.2-gfbf-2023b
+else
+    echo "Don't know which modules to load for ${EESSI_CVMFS_REPO}/versions/${EESSI_VERSION}" >&2
+    exit 1
+fi
+
+time nextflow run ml-hyperopt
