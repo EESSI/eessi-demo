fbe2cf2d9eafe61619f95541fc72099d284fbdb9
Kris Buytaert <574106+KrisBuytaert@users.noreply.github.com>
2026-02-06 10:29:07 +0100

    s/else if/elif/  and improving readability

--- GROMACS/run.sh
+++ GROMACS/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-if [[ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]] && [[ $EESSI_VERSION == "2023.06" ]]; then module load GROMACS/2024.1-foss-2023b
+if [[ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]] && [[ $EESSI_VERSION == "2023.06" ]]; then 
+    module load GROMACS/2024.1-foss-2023b
+elif [[ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]] && [[ $EESSI_VERSION == "2025.06" ]]; then 
+    module load GROMACS/2025.2-foss-2025a
 else 
-  if [[ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]] && [[ $EESSI_VERSION == "2025.06" ]]; then module load GROMACS/2025.2-foss-2025a
-  else 
 	echo "Don't know which GROMACS module to load for ${EESSI_CVMFS_REPO}/versions/${EESSI_VERSION}" >&2; exit 1
-  fi
-fi 
+fi
 
 if [ ! -f GROMACS_TestCaseA.tar.gz ]; then
   curl -OL https://repository.prace-ri.eu/ueabs/GROMACS/1.2/GROMACS_TestCaseA.tar.gz
