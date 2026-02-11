f661c74a66b07a8e5c3feafedf6a222abb6b38c0
Kris Buytaert <574106+KrisBuytaert@users.noreply.github.com>
2026-02-11 09:30:48 +0100

    Adding support for 2025-2 repo

--- Bioconductor/run.sh
+++ Bioconductor/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
-if [[ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]] && [[ $EESSI_VERSION == "2023.06" ]]; then module load R-bundle-Bioconductor/3.16-foss-2022b-R-4.2.2
+if [[ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]] && [[ $EESSI_VERSION == "2023.06" ]]; then 
+    module load R-bundle-Bioconductor/3.16-foss-2022b-R-4.2.2
+elif [[ $EESSI_CVMFS_REPO == "/cvmfs/software.eessi.io" ]] && [[ $EESSI_VERSION == "2025.06" ]]; then 
+    module load R-bundle-Bioconductor/3.20-foss-2024a-R-4.4.2
 else echo "Don't know which Bioconductor module to load for ${EESSI_CVMFS_REPO}/versions/${EESSI_VERSION}" >&2; exit 1
 fi
 
